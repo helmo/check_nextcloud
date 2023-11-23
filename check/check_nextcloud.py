826642845e8d7438e8700c577c15965f83636ce2
Herman van Rink <rink@initfour.nl>
2023-11-23 08:30:05 +0100

    Python3 fix

--- check/check_nextcloud.py
+++ check/check_nextcloud.py
@@ -327,7 +327,7 @@ if options.check == 'uploadFilesize':
 	upload_max_filesize = calc_size_suffix(xml_php_upload_max_filesize)
 
 	if convert_size_to_bytes(upload_max_filesize) >= convert_size_to_bytes(options.upload_filesize):
-		print 'OK - Upload max filesize: {0} >= {1}'.format(upload_max_filesize, options.upload_filesize)
+		print('OK - Upload max filesize: {0} >= {1}'.format(upload_max_filesize, options.upload_filesize))
 		sys.exit(0)		
 	else:
 		print('CRITICAL - Upload max filesize is set to {0}, but should be {1}'.format(upload_max_filesize, options.upload_filesize))
